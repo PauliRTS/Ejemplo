309d80899a358f08fddd56af87726969b71d6830
PauliRTS <PauliRTS@gmail.com>
2025-04-26 12:11:20 -0600

    Quicksort ok

--- Ordenamientos/Quicksort.java
+++ Ordenamientos/Quicksort.java
@@ -1,20 +1,20 @@
 public class Quicksort{
     public static void main (String[] args){
 
-        int[] a={8,2,4,10,9,5,-2,7,6};
+        int[] a={8,2,4,10,9,5,-2,7,6,0,1,11};
 
         imprimirArreglo(a);
-        separar(a,8);
+        quicksort(a,0,a.length-1);
         imprimirArreglo(a);
 
     }
 
-    public static void separar(int[]arreglo, int iPivote){
+    public static int separar(int[]arreglo, int inicio, int iPivote){
         
         int valorPivote = arreglo[iPivote];
-        int i = 0;
+        int i = inicio;
         
-        for( int j= i; j < arreglo.length; j++){
+        for( int j= i; j < iPivote; j++){
             if(arreglo[j] < valorPivote){
                 cambiar(arreglo,i,j);
                 i++;
@@ -23,9 +23,21 @@ public class Quicksort{
         }
 
         cambiar(arreglo,i,iPivote);
-
+        return i;
     }
     
+    public static void quicksort(int[] arreglo, int inicio, int iPivote){
+        
+        if(inicio<iPivote){
+
+        int mitad = separar(arreglo,inicio,iPivote);
+        
+        quicksort(arreglo,inicio,mitad-1);
+        quicksort(arreglo,mitad+1,iPivote);
+
+        }
+    }
+
     public static void cambiar(int[] arreglo, int i, int j){
 
         int temp=arreglo[i];
